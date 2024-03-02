fc240047e7530df3a352a3691ea83771ae4c67a6
Lee Sun-Hyoup <kciter@naver.com>
2024-03-02 23:21:27 +0900

    Add matrix22 class

---
+++ src/matrix22.ts
@@ -0,0 +1,40 @@
+class Matrix22 {
+  m00: number;
+  m01: number;
+  m10: number;
+  m11: number;
+
+  constructor(m00: number, m01: number, m10: number, m11: number) {
+    this.m00 = m00;
+    this.m01 = m01;
+    this.m10 = m10;
+    this.m11 = m11;
+  }
+
+  add(matrix: Matrix22) {
+    return new Matrix22(
+      this.m00 + matrix.m00,
+      this.m01 + matrix.m01,
+      this.m10 + matrix.m10,
+      this.m11 + matrix.m11
+    );
+  }
+
+  subtract(matrix: Matrix22) {
+    return new Matrix22(
+      this.m00 - matrix.m00,
+      this.m01 - matrix.m01,
+      this.m10 - matrix.m10,
+      this.m11 - matrix.m11
+    );
+  }
+
+  multiply(scalar: number) {
+    return new Matrix22(
+      this.m00 * scalar,
+      this.m01 * scalar,
+      this.m10 * scalar,
+      this.m11 * scalar
+    );
+  }
+}
