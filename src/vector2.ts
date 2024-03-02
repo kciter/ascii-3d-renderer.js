2633d8010ad529d621aab9a0ea23241d20475dbd
Lee Sun-Hyoup <kciter@naver.com>
2024-03-02 23:18:11 +0900

    Add vector3 class

--- src/vector2.ts
+++ src/vector2.ts
@@ -1,5 +1,11 @@
 class Vector2 {
-  constructor(public x: number, public y: number) {}
+  x: number;
+  y: number;
+
+  constructor(x: number, y: number) {
+    this.x = x;
+    this.y = y;
+  }
 
   add(vector: Vector2) {
     return new Vector2(this.x + vector.x, this.y + vector.y);
---
+++ src/vector3.ts
@@ -0,0 +1,23 @@
+class Vector3 {
+  x: number;
+  y: number;
+  z: number;
+
+  constructor(x: number, y: number, z: number) {
+    this.x = x;
+    this.y = y;
+    this.z = z;
+  }
+
+  add(v: Vector3): Vector3 {
+    return new Vector3(this.x + v.x, this.y + v.y, this.z + v.z);
+  }
+
+  subtract(v: Vector3): Vector3 {
+    return new Vector3(this.x - v.x, this.y - v.y, this.z - v.z);
+  }
+
+  multiply(s: number): Vector3 {
+    return new Vector3(this.x * s, this.y * s, this.z * s);
+  }
+}
